37c3d84e8e406dc0df02f93f4425cf4f3fff24d6
Simona Cinti <simona.cinti2@gmail.com>
2021-02-01 17:18:19 +0100

    auth

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return 'dashboard home admin';
+        return view('admin.home');
     }
 }
